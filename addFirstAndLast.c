c9392c8fa46151823ae5eb38891486ff1bf4bd5e
Makin <53996363+makinahmed@users.noreply.github.com>
2021-03-24 23:31:49 +0600

    Add files via upload

---
+++ addFirstAndLast.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int i, n;
+//     char num[5];
+
+//     scanf("%d", &n);
+
+//     for (i = 0; i <= n; i++)
+//     {
+//         scanf("%d", num);
+//         printf("%s\n", num[0] + num[4]);
+//     }
+// }
