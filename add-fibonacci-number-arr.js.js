ea2914adfbf4036f8c4b330531a1d7245cd5acfd
Makin <53996363+makinahmed@users.noreply.github.com>
2021-08-06 22:18:19 +0600

    Add files via upload

---
+++ add-fibonacci-number-arr.js.js
@@ -0,0 +1,12 @@
+
+// fibonacci 
+
+// let arr = [0, 1, 1];
+
+// for (let i = 2; i <= 10; i++) {
+//     arr[i] = arr[i - 1] + arr[i - 2] // i-1 = 1 ; i - 2 = 0 ; 1 + 0 = 1;
+// }
+
+// console.log(arr);
+
+
---
+++ convert-c2f-f2c.js
@@ -0,0 +1,25 @@
+
+
+// celcius to fahrenheit 
+
+// function celciusToFahrenheit(temp, scale) {
+//     if (scale === 'F') {
+//         console.log((Math.floor((temp * 1.8) + 32)));
+//     }
+//     else if (scale === 'C') {
+//         console.log(Math.floor(((temp - 32) * (5 / 9))));
+//     }
+
+// }
+
+// celciusToFahrenheit(300, 'F')
+
+
+// simple interest formula
+
+
+// function interest(p, r, t) {
+//     return p * (1 + r * t);
+// }
+
+// console.log(interest(2,2,2));
